d1c08b58937073f18dc846de617c11c838fcd77d
Jacobo Garcia <juanjacobogarcia4@gmail.com>
2024-07-07 11:56:02 -0500

    Add 3.3 testing

--- 3.3-testing/exercises/exercises_test.go
+++ 3.3-testing/exercises/exercises_test.go
@@ -37,7 +37,6 @@ func TestCalculateTax_GreaterThan50000(t *testing.T) {
 	require.Equal(t, expected, result)
 }
 
-
 func TestCalculateAverage(t *testing.T) {
 
 	//Arrange
@@ -52,14 +51,12 @@ func TestCalculateAverage(t *testing.T) {
 	require.Equal(t, expected, result)
 }
 
-
 // Categoría C, su salario es de $1.000 por hora.
 // Categoría B, su salario es de $1.500 por hora, más un 20 % de su salario mensual.
 // Categoría A, su salario es de $3.000 por hora, más un 50 % de su salario mensual.
 
-
 func TestCalculateSalary_CategoryC(t *testing.T) {
-	
+
 	//Arrange
 	const (
 		minutes  = 60
@@ -74,9 +71,8 @@ func TestCalculateSalary_CategoryC(t *testing.T) {
 	require.Equal(t, expected, result)
 }
 
-
 func TestCalculateSalary_CategoryB(t *testing.T) {
-	
+
 	//Arrange
 	const (
 		minutes  = 60
@@ -91,10 +87,8 @@ func TestCalculateSalary_CategoryB(t *testing.T) {
 	require.Equal(t, expected, result)
 }
 
-
-
 func TestCalculateSalary_CategoryA(t *testing.T) {
-	
+
 	//Arrange
 	const (
 		minutes  = 60
@@ -110,7 +104,7 @@ func TestCalculateSalary_CategoryA(t *testing.T) {
 }
 
 func TestCalculateSalary_InvalidCategory(t *testing.T) {
-	
+
 	//Arrange
 	const (
 		minutes  = 60
@@ -125,8 +119,122 @@ func TestCalculateSalary_InvalidCategory(t *testing.T) {
 	require.Equal(t, expected, result)
 }
 
+func TestOperation_Minimum(t *testing.T) {
 
+	//Arrange
+	const (
+		op       = "minimum"
+		expected = 1
+	)
 
+	//Act
+	result, _ := exercises.Operation(op)
 
+	//Assert
+	require.Equal(t, expected, result(1, 2, 3, 4, 5))
+}
 
+func TestOperation_Average(t *testing.T) {
 
+	//Arrange
+	const (
+		op       = "average"
+		expected = 3
+	)
+
+	//Act
+	result, _ := exercises.Operation(op)
+
+	//Assert
+	require.Equal(t, expected, result(1, 2, 3, 4, 5))
+}
+
+func TestOperation_Maximum(t *testing.T) {
+
+	//Arrange
+	const (
+		op       = "maximum"
+		expected = 5
+	)
+
+	//Act
+	result, _ := exercises.Operation(op)
+
+	//Assert
+	require.Equal(t, expected, result(1, 2, 3, 4, 5))
+}
+
+func TestOperation_InvalidOperation(t *testing.T) {
+
+	//Arrange
+	const (
+		op       = "invalid"
+		expected = "Operation invalid not defined"
+	)
+
+	//Act
+	_, err := exercises.Operation(op)
+
+	//Assert
+	require.EqualError(t, err, expected)
+}
+
+func TestAnimal_Dog(t *testing.T) {
+
+	//Arrange
+	const (
+		quantity = 10
+		expected = 100000.00
+	)
+
+	//Act
+	result, _ := exercises.Animal("dog")
+
+	//Assert
+	require.Equal(t, expected, result(quantity))
+}
+
+func TestAnimal_Cat(t *testing.T) {
+
+	//Arrange
+	const (
+		quantity = 10
+		expected = 50000.00
+	)
+
+	//Act
+	result, _ := exercises.Animal("cat")
+
+	//Assert
+	require.Equal(t, expected, result(quantity))
+}
+
+func TestAnimal_Hamster(t *testing.T) {
+
+	//Arrange
+	const (
+		quantity = 10
+		expected = 2500.00
+	)
+
+	//Act
+	result, _ := exercises.Animal("hamster")
+
+	//Assert
+	require.Equal(t, expected, result(quantity))
+}
+
+func TestAnimal_Tarantula(t *testing.T) {
+
+	//Arrange
+	const (
+		quantity = 10
+		expected = 1500.00
+	)
+
+	//Act
+	result, _ := exercises.Animal("tarantula")
+
+	//Assert
+	require.Equal(t, expected, result(quantity))
+}
